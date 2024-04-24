e0a8eb2234279e20a9ab3ac97861da5008999e39
JessicaDin44 <157174338+JessicaDin44@users.noreply.github.com>
2024-04-24 15:34:12 -0400

    Update eeg_load_examine.m

--- Scripts/eeg_load_examine.m
+++ Scripts/eeg_load_examine.m
@@ -8,4 +8,4 @@ headerFile={
 eeg{1} = double(bva_loadeeg(headerFile{1}));
 
 % meta information such as samplingRate (fs), labels, etc
-[fs(1) label meta] = bva_readheader(headerFile{1}
+[fs(1) label meta] = bva_readheader(headerFile{1})
