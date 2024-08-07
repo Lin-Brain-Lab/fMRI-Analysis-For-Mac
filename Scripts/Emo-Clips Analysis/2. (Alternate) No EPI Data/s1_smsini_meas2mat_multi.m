76a6774cfef2ad98176f6823eed1e94cb457dc56
JessicaDin44 <157174338+JessicaDin44@users.noreply.github.com>
2024-08-07 16:31:52 -0400

    Create s1_smsini_meas2mat_multi.m

---
+++ Scripts/Emo-Clips Analysis/2. (Alternate) No EPI Data/s1_smsini_meas2mat_multi.m
@@ -0,0 +1,35 @@
+close all; clear all;
+
+pdir=pwd;
+
+
+addpath('/Users/jessica/Documents/MBP_PhD/Lin_Lab/fhlin_toolbox/codes/tool_mb_recon/VD');
+addpath('/Users/jessica/Documents/MBP_PhD/Lin_Lab/fhlin_toolbox/codes/tool_mb_recon/');
+
+%path_dat_source={'/Users/jessica/data_analysis/emoclips/s001/fmri_raw/meas/';
+%};
+path_dat_source={'/Users/jessica/data_analysis/emoclips/s005/fmri_raw/meas/';
+};
+
+path_mat_destination='/Users/jessica/data_analysis/emoclips/s005/fmri_analysis/';
+
+n_dummy=0; %number of dummy scans
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if(~exist('n_dummy')) n_dummy=0; end;
+if(isempty(n_dummy)) n_dummy=0; end;
+
+for f_idx=1:length(path_dat_source)
+        ref_files=dir(sprintf('%s/*MBSIREPI*ref*.dat',path_dat_source{f_idx}));
+        acc_files=dir(sprintf('%s/*MBSIREPI*acc*.dat',path_dat_source{f_idx}));
+
+        for i=1:length(acc_files)
+                [ ref,EPInew ] = MBrecon_VE( [path_dat_source{f_idx},ref_files(i).name],[path_dat_source{f_idx},acc_files(i).name],0.0005 );
+
+                %[ ref,EPInew ] = MBrecon( [path_dat_source{f_idx},ref_files(i).name],[path_dat_source{f_idx},acc_files(i).name],0.0005 );
+                acc=abs(EPInew(:,:,:,n_dummy+1:end));
+                %acc=abs(EPInew);
+                save(sprintf('%s/mb_run_%s_ref.mat',path_mat_destination,num2str(i)),'ref','-v7.3');
+                save(sprintf('%s/mb_run_%s_acc.mat',path_mat_destination,num2str(i)),'acc','-v7.3');
+        end
+end;
