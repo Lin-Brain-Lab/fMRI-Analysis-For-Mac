e8dd5b8825c2f71bd10757a74a032d5f3705df2e
JessicaDin44 <157174338+JessicaDin44@users.noreply.github.com>
2024-10-08 15:37:44 -0400

    Update s5.1_corrected_onset.m

--- Scripts/Emo-Clips Analysis/3. GLM/s5.1_corrected_onset.m
+++ Scripts/Emo-Clips Analysis/3. GLM/s5.1_corrected_onset.m
@@ -1,15 +1,15 @@
 close all; clear all;
 
 headerFile = {
-%   '../eeg_raw/s001_emoclips_0001.vhdr';
+    '../eeg_raw/s001_emoclips_0001.vhdr';
     '../eeg_raw/s001_emoclips_0002.vhdr';
-%   '../eeg_raw/s001_emoclips_0003.vhdr';
+    '../eeg_raw/s001_emoclips_0003.vhdr';
     };
 
 markerFile={
-%   '../eeg_raw/s001_emoclips_0001.vmrk';
+    '../eeg_raw/s001_emoclips_0001.vmrk';
     '../eeg_raw/s001_emoclips_0002.vmrk';
-%   '../eeg_raw/s001_emoclips_0003.vmrk';
+    '../eeg_raw/s001_emoclips_0003.vmrk';
     };
 erp_event={1e3, [1 2 3 4 5], [11 12 13 14 15 16 17], [51 52 53 54 55 56 57 58], [61 62 63 64 65 66 67 68 69 70 71 72], [81 82 83 84 85 86 87 88 89], [90 91 92 93 94 95 96 97 98 99], [100 101 102 103 104 105 106 107 108 109], [111 112 113 114 115 116 117 118]};
 
